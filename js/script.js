b57f798d0536a6b8c93b57917634de82fab4abb1
Simon Baird <simon.baird@gmail.com>
2010-10-10 19:22:07 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+/* Author: 
+
+*/
+;(function($){
+
+var cross    = '\u25EF',
+	nought   = '\u2715',
+	blank    = '',
+	whosTurn = cross;
+
+function turnRotate() {
+	whosTurn = (whosTurn == cross) ? nought : cross;
+}
+
+$(function(){
+
+	$.each(["2_1","1_2","2_2","3_2","2_3","2_4"],function(index,value){
+		$('#boardTemplate').tmpl([{'index':'1'}]).appendTo('#'+value);
+	});
+
+	$('#board a').hover(
+		function() {
+			if (!$(this).hasClass('played')) {
+				$(this).html(whosTurn == cross ? cross : nought);
+				$(this).addClass('hover');
+			}
+		},
+		function() {
+			if ($(this).hasClass('hover')) {
+				$(this).html('');
+				$(this).removeClass('hover');
+			}
+		}
+	);
+
+	$('#board a').click(function(){
+		turnRotate();
+		$(this).removeClass('hover');
+		$(this).addClass('played');
+		return false;
+	});
+
+	$('#reset').click(function(){
+		$('#board a').html('');
+		$('#board a').removeClass('played');
+		return false;
+	});
+
+
+});
+
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
