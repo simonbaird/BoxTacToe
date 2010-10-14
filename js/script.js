a583dc6147c1c4d340d8cb70fae732ec9b0c3a8d
Simon Baird <simon.baird@gmail.com>
2010-10-14 20:07:38 +1000

    detect win wip

--- js/script.js
+++ js/script.js
@@ -13,18 +13,40 @@ function turnRotate() {
 }
 
 function Board() {
+	this.faces = ['top','north','south','east','west','base'];
+
+	this.squares = {
+		'top':  [[],[],[]],
+		'north':[[],[],[]],
+		'south':[[],[],[]],
+		'east': [[],[],[]],
+		'west': [[],[],[]],
+		'base': [[],[],[]]
+	};
+
 	return this;
 }
 
 $.extend(Board.prototype,{
+	same: function(first,second,third) {
+		if (first == second && first == third) return first;
+		return false;
+	},
+
 	gameOver: function() {
+		for (var square in this.squares) {
+			winner = this.same(this.squares[square][0][0],this.squares[square][1][1],this.squares[square][2][2]);
+			if (winner) return winner;
+			winner = this.same(this.squares[square][2][0],this.squares[square][1][1],this.squares[square][0][2]);
+			if (winner) return winner;
+		}
 		return false;
 	},
 
 	movePlayed: function(position_string,player) {
-		//console.log(position_string + ": " + player);
+		console.log(position_string);
 		position = position_string.split('_');
-		// todo
+		this.squares[position[0]][position[1]][position[2]] = player;
 	}
 });
 
